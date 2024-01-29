5cb225260bb701dbd4c7120c3b0bc9c8a0274a65
Bayu Krisnawan <krisna@gmail.com>
2024-01-29 08:39:23 +0700

    remove freeswitch

--- freeswitch/build/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$1" = 'freeswitch' ] && [ -f "/letsencrypt/certs/cert.pem" ]; then
-  /usr/bin/freeswitch -nonat -c -rp
-fi
-
-exec "$@"
--- freeswitch/build/entrypoint.centos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-if [ -z "$@" ]; then
-  /usr/bin/supervisord -c /etc/supervisord.conf  --nodaemon
-else
-  PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
-fi
