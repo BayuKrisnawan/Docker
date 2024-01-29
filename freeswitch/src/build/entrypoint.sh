f9371dd71898e9e52476fff23d91be1d5b29a010
Bayu Krisnawan <krisna@gmail.com>
2024-01-29 08:59:06 +0700

    Hmmm

--- freeswitch/src/build/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-if [ -z "$@" ]; then
-  /usr/bin/supervisord -c /etc/supervisor/supervisord.conf  --nodaemon --logfile=/proc/1/fd/1 --logfile_maxbytes=0
-else
-  PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
-fi
